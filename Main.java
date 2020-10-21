266fbf4eb565286a6b26d62dc345ca2a87d2b0cf
Atharva Ashok Patil <39415890+Attupatil@users.noreply.github.com>
2020-10-21 19:00:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Main {
 
@@ -8,16 +8,63 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String message1 = scanner.nextLine();
-        String message = scanner.nextLine();
-        int key = scanner.nextInt();
-        if (message1.startsWith("enc")){
-            System.out.println(caesarEncrypt(message, key));
-        }else if(message1.startsWith("dec")){
-            System.out.println(caesarDecrypt(message, key));
+        // Scanner scanner = new Scanner(System.in);
+        // String message1 = scanner.nextLine();
+        // String message = scanner.nextLine();
+        // int key = scanner.nextInt();
+        // if (message1.startsWith("enc")){
+        //     System.out.println(caesarEncrypt(message, key));
+        // }else if(message1.startsWith("dec")){
+        //     System.out.println(caesarDecrypt(message, key));
+        // }
+        
+        
+        // String algorithm = "shift";
+        String mode = "enc";
+        String data = "";
+        String source = null;
+        String output = null;
+        int key = 0;
+
+    for (int i = 0; i < args.length - 1; i++) {
+        switch(args[i]) {
+            case "-mode":
+                if (args[i+1].toLowerCase().equals("dec")) {
+                    mode = "dec";
+                }
+                break;
+                // case "-alg":
+                //     if (args[i+1].toLowerCase().equals("unicode")) {
+                //         algorithm = "unicode";
+                //     }
+                //     break;
+            case "-key":
+                try {
+                    key = Integer.parseInt(args[i+1]);
+                } catch (NumberFormatException e) {
+                    key = 0;
+                }
+                break;
+            case "-data":
+                data = args[i+1];
+                break;
+                // case "-in":
+                //     source = args[i+1];
+                //     break;
+                // case "-out":
+                //     output = args[i+1];
+                //     break;
         }
 
+    }
+        String message1 = mode;
+        String message = data;
+        if (message1.startsWith("enc")){
+            System.out.println(caesarEncrypt(message, key));
+        }
+        else if(message1.startsWith("dec")){
+            System.out.println(caesarDecrypt(message, key));
+        }
     }
 
     public static String caesarEncrypt(String str, int key) {
@@ -37,12 +84,12 @@ public class Main {
 
         for (char c : str.toCharArray()) {
             
-                char newChar = (char) (c - key );
-                decoded.append(newChar);
+            char newChar = (char) (c - key );
+            decoded.append(newChar);
             
         }
         decoded.append("\n");
         return decoded.toString();
 
-}
+    }
 }
