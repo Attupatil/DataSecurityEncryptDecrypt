2975941c6bc827f94825354f917fc378ac6e3df9
Atharva Patil <39415890+Attupatil@users.noreply.github.com>
2020-11-12 19:07:03 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,30 +9,15 @@
  * @clone_url: "https://github.com/Attupatil/DataSecurityEncryptDecrypt.git",
  * @svn_url: "https://github.com/Attupatil/DataSecurityEncryptDecrypt",
  */
-// import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.PrintWriter;
 
 
-public class Og {
-
-
+public class Main {
 
     public static void main(String[] args) {
-
-        // Scanner scanner = new Scanner(System.in);
-        // String message1 = scanner.nextLine();
-        // String message = scanner.nextLine();
-        // int key = scanner.nextInt();
-        // if (message1.startsWith("enc")){
-        //     System.out.println(caesarEncrypt(message, key));
-        // }else if(message1.startsWith("dec")){
-        //     System.out.println(caesarDecrypt(message, key));
-        //
-
-
         String algorithm = "unicode";
         String mode = "enc";
         String data = "";
@@ -71,7 +56,6 @@ public class Og {
             }
             if (source != null) {
                         data = inputFromFile(source);
-
             }
         }
 
@@ -90,10 +74,7 @@ public class Og {
         else if(message1.startsWith("enc") && algorithm.startsWith("shift")){
                     System.out.println(displayResult(Shiftdec(message, key),output));
         }
-
-
     }
-
 
     public static String caesarEncrypt(String str, int key) {
         StringBuilder encoded = new StringBuilder();
@@ -148,12 +129,8 @@ public class Og {
                     return outputToFile(data, output);
                 }
             }
-
-
-
-
-
-         public static String Shiftenc(String data, int key){
+    
+    public static String Shiftenc(String data, int key){
 
              String result = "";
                  for (int character : data.toCharArray()) {
@@ -198,140 +175,4 @@ public class Og {
                  }
                  return result;
              } 
-
-             // public static String Shiftenc(String message, int key) {
-             //         int shiftRange = 'z' - 'a' + 1;
-             //         char[] msgCharArray = message.toCharArray();
-             //         int msgLength = msgCharArray.length;
-             //         char[] encryptedMessage = new char[msgLength];
-             //         for (int i = 0; i < msgLength; i++) {
-             //             char c = msgCharArray[i];
-             //             if (Character.isLetter(c)) {
-             //                 char e = (char) (c + key);
-             //                 if ((Character.isLowerCase(c) && e > 'z') || (Character.isUpperCase(c) && e > 'Z')) {
-             //                     e -= shiftRange;
-             //                 }
-             //                 if ((Character.isLowerCase(c) && e < 'a') || (Character.isUpperCase(c) && e < 'A')) {
-             //                     e += shiftRange;
-             //                 }
-             //                 encryptedMessage[i] = e;
-             //             } else {
-             //                 encryptedMessage[i] = c;
-             //             }
-             //         }
-             //         return new String(encryptedMessage);
-             //     }
-
-
-             //     public static String Shiftdec(String message, int key) {
-             //         return Shiftenc(message, -key);
-             //     }
-    //     public static void Shiftenc(String text, int key, FileWriter writer) throws IOException {
-    //     String referenceForSymbols = " !\"#$%&'()*+,-./0123456789:;<=>?[\\]^_`{|}~";
-    //     String referenceForCharactersLower = "abcdefghijklmnopqrstuvwxyz";
-    //     String referenceForCharactersUpper = "ABCDEFGHIJKLMNOQPRSTUVWXYZ";
-    //     String encryptedReferenceForCharsLower = referenceForCharactersLower.substring(key) + referenceForCharactersLower.substring(0, key);
-    //     String encryptedReferenceForCharsUpper = referenceForCharactersUpper.substring(key) + referenceForCharactersUpper.substring(0, key);
-    //     String character;
-    //     StringBuilder newText = new StringBuilder();
-    //     for (int i = 0; i < text.length(); i++) {
-    //         for (int k = 0; k < referenceForSymbols.length(); k++) {
-    //             if (text.charAt(i) == referenceForSymbols.charAt(k)) {
-    //                 newText.append(referenceForSymbols.charAt(k));
-    //                 break;
-    //             }
-    //         }
-    //         for (int j = 0; j < encryptedReferenceForCharsLower.length(); j++) {
-    //             if (text.charAt(i) == referenceForCharactersLower.charAt(j)) {
-    //                 character = (text.replace(text.charAt(i), encryptedReferenceForCharsLower.charAt(j)));
-    //                 newText.append(character.charAt(i));
-    //                 break;
-    //             }
-    //         }
-    //         for (int j = 0; j < encryptedReferenceForCharsUpper.length(); j++) {
-    //             if (text.charAt(i) == referenceForCharactersUpper.charAt(j)) {
-    //                 character = (text.replace(text.charAt(i), encryptedReferenceForCharsUpper.charAt(j)));
-    //                 newText.append(character.charAt(i));
-    //                 break;
-    //             }
-    //         }
-    //     }
-    //     //String newerText= newText.toString().u
-    //     try (writer) {
-    //         if (writer != null) {
-    //             writer.write(String.valueOf(newText));
-    //         } else {
-    //             System.out.println(newText);
-    //         }
-    //     }
-    // }
-    //     public static void Shiftdec(String text, int key, FileWriter writer) throws IOException {
-    //     String referenceForSymbols = " !\"#$%&'()*+,-./0123456789:;<=>?[\\]^_`{|}~";
-    //     StringBuilder referenceForCharactersLower =new StringBuilder( "abcdefghijklmnopqrstuvwxyz")  ;
-    //     StringBuilder referenceForCharactersUpper =new StringBuilder("ABCDEFGHIJKLMNOQPRSTUVWXYZ")    ;
-    //     String encryptedReferenceForCharsLower = referenceForCharactersLower.substring(key) + referenceForCharactersLower.substring(0, key);
-    //     String encryptedReferenceForCharsUpper = referenceForCharactersUpper.substring(key) + referenceForCharactersUpper.substring(0, key);
-    //     String character;
-    //     StringBuilder newText = new StringBuilder();
-    //     if (key == 0) {
-    //         newText.append(text);
-    //     } else {
-    //         for (int i = 0; i < text.length(); i++) {
-    //             for (int k = 0; k < referenceForSymbols.length(); k++) {
-    //                 if (text.charAt(i) == referenceForSymbols.charAt(k)) {
-    //                     newText.append(referenceForSymbols.charAt(k));
-    //                     break;
-    //                 }
-    //             }
-    //             int temp=0;
-    //             int tempj=0;
-    //             for (int j = 0; j < referenceForCharactersLower.length(); j++) {
-    //                 tempj = j;
-    //                 if (referenceForCharactersLower.charAt(0)=='z'){
-    //                     referenceForCharactersLower.reverse();
-    //                 }
-    //                 if (text.charAt(i) == referenceForCharactersLower.charAt(j)) {
-    //                     if (j<key){ //3-10
-    //                         temp =key - j-1;
-    //                         // referenceForCharactersLower.reverse();
-    //                         // temp = j;
-    //                         //  j= referenceForCharactersLower.length()-2; // 25-2
-    //                         //   j-=2;
-    //                         character = (text.replace(text.charAt(i), referenceForCharactersLower.reverse().charAt(temp)));
-    //                     }else {
-    //                         // j=temp;
-    //                         character = (text.replace(text.charAt(i), referenceForCharactersLower.charAt(j-key)));
-    //                     }
-    //                     newText.append(character.charAt(i));
-    //                     break;
-    //                 }
-    //             }
-    //             for (int j = 0; j < referenceForCharactersUpper.length(); j++) {
-    //                 if (text.charAt(i) == referenceForCharactersUpper.charAt(j)) {
-    //                     if (j-key<0){
-    //                         j= referenceForCharactersUpper.length()-2;
-    //                         j-=2;
-    //                         character = (text.replace(text.charAt(i), referenceForCharactersUpper.charAt(j)));
-    //                     }else {
-    //                         character = (text.replace(text.charAt(i), referenceForCharactersUpper.charAt(j-key)));
-    //                     }
-    //                     newText.append(character.charAt(i));
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     try (writer) {
-    //         if (writer != null) {
-    //             writer.write(String.valueOf(newText));
-    //         } else {
-    //             System.out.println(newText);
-    //         }
-    //     }
-    // }
-
-
-
-
-
 }
